970548b9a4d814d1cd23f8cd5075fa316d053c29
Qunainb <waniqunain@gmail.com>
2025-06-16 05:09:47 +0530

    Set up the Sidebar Context

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+import { IoMdArrowForward } from "react-icons/io";
+import { FiTrash2 } from "react-icons/fi";
+
+import CartItem from "./CartItem";
+import { SidebarContext } from "../contexts/SidebarContext";
+import { useContext } from "react";
+
 export default function Sidebar() {
+  const { isOpen, handleClose } = useContext(SidebarContext);
+
   return <div>Sidebar</div>;
 }
--- src/contexts/SidebarContext.jsx
+++ src/contexts/SidebarContext.jsx
@@ -1,3 +1,23 @@
-export default function CartContext() {
-  return <div>SidebarContext</div>;
+import { createContext, useState } from "react";
+
+export const SidebarContext = createContext({});
+
+export default function SidebarContextProvider({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleClose() {
+    setIsOpen(false);
+  }
+
+  return (
+    <SidebarContext.Provider
+      value={{
+        isOpen,
+        setIsOpen,
+        handleClose,
+      }}
+    >
+      {children}
+    </SidebarContext.Provider>
+  );
 }
